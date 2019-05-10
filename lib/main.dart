b1f89311ab57c7c090e3f0e79ff2238b9307355f
sunny <sunnykrditya@gmail.com>
2019-05-10 23:21:31 +0530

    edit text box

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    new MaterialApp(
-      title: "Friendlychat",
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Friendly chat"),
-        ),
-      ),
-    ),
+    new FriendlychatApp(),
   );
 }
 
@@ -22,12 +15,82 @@ class FriendlychatApp extends StatelessWidget {
     );
   }
 }
-
-class ChatScreen extends StatelessWidget {
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text});
+  final String text;
   @override
+  Widget build(BuildContext context) {
+    return new Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: new Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          new Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: new CircleAvatar(child: new Text(_name[0])),
+          ),
+          new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Text(_name, style: Theme.of(context).textTheme.subhead),
+              new Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: new Text(text),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  //modified
+  @override //new
+  State createState() => new ChatScreenState(); //new
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = new TextEditingController();
+
+  Widget _buildTextComposer() {
+    return new IconTheme(                                            //new
+      data: new IconThemeData(color: Theme.of(context).accentColor), //new
+      child: new Container(                                     //modified
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: new Row(
+          children: <Widget>[
+            new Flexible(
+              child: new TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration: new InputDecoration.collapsed(
+                    hintText: "Send a message"),
+              ),
+            ),
+            new Container(
+              margin: new EdgeInsets.symmetric(horizontal: 4.0),
+              child: new IconButton(
+                  icon: new Icon(Icons.send),
+                  onPressed: () => _handleSubmitted(_textController.text)),
+            ),
+          ],
+        ),
+      ),                                                             //new
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
+
+  //new
+  @override //new
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(title: new Text("Friendlychat")),
+      body: _buildTextComposer(),
     );
   }
 }
